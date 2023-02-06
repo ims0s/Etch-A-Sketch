1790e2f5da239537180abb043a5c458b4242290f
ims0's laptop <soruor2001@gmail.com>
2023-02-06 02:56:01 +0200

    create reset button

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const grid=document.getElementById("grid");
 const colorPicker= document.getElementById('color')
+const reset=document.getElementById('reset')
 let pixels;
 let color = 'black'
 let mouseDown = false;
@@ -11,7 +12,7 @@ function createGrid(side=16){
     grid.style.gridTemplateRows=`repeat(${side},1fr)`; // create columns
     grid.style.gridTemplateColumns=`repeat(${side},1fr)`;//create rows
     for(let i = 0;i<side*side;i++){
-        const pixel=document.createElement('div') // create each pixel and give it a class and give it 
+        const pixel=document.createElement('div') // create each pixel and give it a class and append it into the grid 
         pixel.classList.add('pixel')
         grid.appendChild(pixel)
         
@@ -24,11 +25,13 @@ function createGrid(side=16){
 
 const changeColor=(e) =>{
 
-    if(mouseDown){
+    if(mouseDown){// here we check if the mouse pressed and hold.
         e.target.style.background=color;
     }
 }
 
-colorPicker.addEventListener('change',(e) => color = colorPicker.value)
+colorPicker.addEventListener('change',(e) => color = colorPicker.value)//when change the color
+
+reset.addEventListener('click',() => createGrid())
 
 createGrid();
