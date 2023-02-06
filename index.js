dfdce3966a8c0dc07a9acc711e5dcf4be589a5c0
ims0's laptop <soruor2001@gmail.com>
2023-02-06 02:34:14 +0200

    clean the script file , let color picker chnage the color when drawing , little styling of the page

--- index.js
+++ index.js
@@ -1,51 +1,34 @@
-let pixels=document.querySelectorAll('div.pixel')
-console.log(pixels)
+const grid=document.getElementById("grid");
+const colorPicker= document.getElementById('color')
+let pixels;
+let color = 'black'
+let mouseDown = false;
+document.body.addEventListener('mousedown',() => mouseDown=true);
+document.body.addEventListener('mouseup',() => mouseDown=false);
 
-console.log()
-
-const grid=document.getElementById("grid")
-// document.getElementById('grid').style.width='1000px'
-// jgrid.style.width='200px'
-// // console.log(hight)
-let mousedown=false
-document.body.addEventListener('mousedown',() => mousedown=true)
-document.body.addEventListener('mouseup',() => mousedown=false)
-
-function addPixels(side){
-    grid.innerHTML=''
-    for(let i =0 ; i<side*side; i++){
-        const pixel=document.createElement('div')
+function createGrid(side=16){
+    grid.innerHTML=''; //here I'm emptying the grid
+    grid.style.gridTemplateRows=`repeat(${side},1fr)`; // create columns
+    grid.style.gridTemplateColumns=`repeat(${side},1fr)`;//create rows
+    for(let i = 0;i<side*side;i++){
+        const pixel=document.createElement('div') // create each pixel and give it a class and give it 
         pixel.classList.add('pixel')
-        pixel.style.width=`${700/side}px`
-        pixel.style.height=`${700/side}px`
         grid.appendChild(pixel)
-        console.log(i)
-    }        
+        
+    }
     pixels=document.querySelectorAll('div.pixel')
-    pixels.forEach(pixel => pixel.addEventListener('mouseover',(e)=>{
-        if(mousedown){
-            console.log(e.target.style.backgroundColor="black")
-            e.target.style.backgroundColor="black"
-        }
-    }))
-    pixels.forEach(pixel => pixel.addEventListener('mousedown',(e)=>{
-        
-            console.log(e.target.style.backgroundColor="black")
-            e.target.style.backgroundColor="black"
-        
-    }))
-    
+    pixels.forEach((pixel) => pixel.addEventListener('mouseover',changeColor));
+    pixels.forEach((pixel) => pixel.addEventListener('mousedown',changeColor));
+
 }
 
-pixels.forEach(pixel => pixel.addEventListener('mouseover',(e)=>{
-    if(mousedown){
-        console.log(e.target.style.backgroundColor="black")
-        e.target.style.backgroundColor="black"
+const changeColor=(e) =>{
+
+    if(mouseDown){
+        e.target.style.background=color;
     }
-}))
-pixels.forEach(pixel => pixel.addEventListener('mousedown',(e)=>{
-    
-        console.log(e.target.style.backgroundColor="black")
-        e.target.style.backgroundColor="black"
-    
-}))
+}
+
+colorPicker.addEventListener('change',(e) => color = colorPicker.value)
+
+createGrid();
